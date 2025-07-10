7f3deb0d8e0b0e48d0987061083f8b6e084cd72f
danhnguyenthanh260 <danhnguyenthanh260@gmail.com>
2025-07-10 19:11:33 +0700

    use env vars for database config

--- pdo.php
+++ pdo.php
@@ -1,6 +1,8 @@
 <?php
-$pdo = new PDO('mysql:host=sql100.infinityfree.com;port=3306;dbname=if0_39436771_autosdb;charset=utf8',
-               'if0_39436771',
-               'q3EiPMTgj8Jg'); // thay bằng mật khẩu thật
+$pdo = new PDO(
+    'mysql:host=' . getenv('DB_HOST') . ';port=3306;dbname=' . getenv('DB_NAME') . ';charset=utf8',
+    getenv('DB_USER'),
+    getenv('DB_PASS')
+);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
